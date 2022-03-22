71fdcbebb12f89f551ebdcb3506983bf55a219fa
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-03-22 18:31:08 +0100

    Started adding events to gui

--- src/java/gui/controllers/CoordinatorController.java
+++ src/java/gui/controllers/CoordinatorController.java
@@ -1,10 +1,13 @@
 package gui.controllers;
 
+import be.Event;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.TilePane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,9 +15,38 @@ import java.util.ResourceBundle;
 public class CoordinatorController implements Initializable {
 
 
+    @FXML public Button btnCreateActions;
+    @FXML public Label lblUser;
+    @FXML public ImageView imgViewUser;
+
+    @FXML public TilePane tilePaneEvents;
+
+    @FXML public ImageView imgViewEvent;
+    @FXML public Button btnEditEvent;
+    @FXML public Label lblEventName;
+    @FXML public Label lblEventDate;
+    @FXML public Label lblEventTime;
+    @FXML public Label lblEventVenue;
+    @FXML public Label lblRemainingTickets;
+    @FXML public Label lblSoldTickets;
+    @FXML public TableView tblViewTicketGroup;
+    @FXML public TableColumn tblClmnGroupName;
+    @FXML public TableColumn tblClmnGroupPrice;
+    @FXML public TextArea txtAreaInfo;
+
     @FXML public TextField txtFieldSearch;
     @FXML public Button btnSearch;
-    @FXML public Button btnCreateActions;
+    @FXML public TableView tblViewAttendees;
+    @FXML public TableColumn tblClmAttFirstName;
+    @FXML public TableColumn tblClmAttLastName;
+    @FXML public TableColumn tblClmAttEmail;
+    @FXML public TableColumn tblClmAttPhone;
+    @FXML public TableColumn tblClmAttTicketGroup;
+    @FXML public TableColumn tblClmAttTicketNo;
+    @FXML public TableColumn tblClmAttSeatNo;
+
+    @FXML public FlowPane flowPaneAttendeeBtns;
+
 
 
     @Override
@@ -23,7 +55,8 @@ public class CoordinatorController implements Initializable {
     }
 
     public void onCreate(ActionEvent event) {
-
+        Event event1 = new Event();
+        tilePaneEvents.getChildren().add(event1.getEventTile());
     }
 
 }
