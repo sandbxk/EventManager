3f105829547ef98eddb8f43bf690451a5cb1446d
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-03-22 13:32:55 +0100

    Removed unused class

--- src/java/gui/controllers/CoordinatorController.java
+++ src/java/gui/controllers/CoordinatorController.java
@@ -1,13 +1,9 @@
 package gui.controllers;
 
-import gui.component.SizeControlledContextMenu;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Side;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
