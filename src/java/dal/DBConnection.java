a11cfa8436ede1d5cdc1a115eaaf68f9750a6a05
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-03-22 08:07:44 +0100

    Update DBConnection.java

--- src/java/dal/DBConnection.java
+++ src/java/dal/DBConnection.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 public class DBConnection
 {
     public static final String DRIVER_CLASS         = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static final String CONNECTION_STRING    = "jdbc:sqlserver://10.176.111.31;database=CSe21A_16_BankAccount";
+    public static final String CONNECTION_STRING    = "jdbc:sqlserver://";
 
-    public static final String USERNAME = "CSe21A_16";
-    public static final String PASSWORD = "CSe21A_16";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
 
 
     public static Connection get() throws ClassNotFoundException, SQLException
