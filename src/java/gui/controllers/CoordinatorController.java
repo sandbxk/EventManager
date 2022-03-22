b34804b7d238109ec193d210bc1f1d807d76e0c3
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-03-22 10:32:01 +0100

    Fininshed for now. Needs FXIDs

--- src/java/gui/controllers/CoordinatorController.java
+++ src/java/gui/controllers/CoordinatorController.java
@@ -1,12 +1,18 @@
 package gui.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CoordinatorController implements Initializable {
+
+
+    @FXML public TextField txtFieldSearch;
+    @FXML public Button btnSearch;
 
 
 
