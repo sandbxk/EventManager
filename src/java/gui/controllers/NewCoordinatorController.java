5ef0417bc72522c8029fb00596357ea571343944
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-03-22 12:24:36 +0100

    newCoordinator in progress and updated stylesheet

---
+++ src/java/gui/controllers/NewCoordinatorController.java
@@ -0,0 +1,12 @@
+package gui.controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewCoordinatorController {
+    public TextField txtLastName;
+    public TextField txtUserName;
+    public TextField txtPassword;
+    public Button btnSubmit;
+    public Button btnCancel;
+}
