1d0f5e029e7db815ea3402b00fa29118e5a068f4
Mads Mandahl-Barth <mads-barth@live.dk>
2022-03-24 19:56:29 +0100

    fixed deps, we can now generate pdf files

--- src/java/bll/ExporterPDF.java
+++ src/java/bll/ExporterPDF.java
@@ -2,19 +2,28 @@ package bll;
 
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
+import j2html.TagCreator;
 
 import java.io.*;
+import java.util.stream.Stream;
 
 public class ExporterPDF
 {
     public void export() throws IOException
     {
-        File htmlSource = new File("input.html");
+        var html = TagCreator.body(
+                TagCreator.h1("Hello, World!")
+        ).render();
+
         File pdfDest = new File("output.pdf");
 
         // pdfHTML specific code
         ConverterProperties converterProperties = new ConverterProperties();
-        HtmlConverter.convertToPdf(new FileInputStream(htmlSource),
-                new FileOutputStream(pdfDest), converterProperties);
+        converterProperties.setImmediateFlush(true);
+        HtmlConverter.convertToPdf(new ByteArrayInputStream(html.getBytes()), new FileOutputStream(pdfDest), converterProperties);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ExporterPDF().export();
     }
 }
