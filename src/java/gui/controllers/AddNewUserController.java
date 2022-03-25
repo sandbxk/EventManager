19545878dbf3c29ca9eba385aa83bc9ba3f5fa5c
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-03-25 16:07:32 +0100

    Start of Manager ability to add new User

---
+++ src/java/gui/controllers/AddNewUserController.java
@@ -0,0 +1,16 @@
+package gui.controllers;
+
+import javafx.event.ActionEvent;
+
+public class AddNewUserController {
+
+
+
+    public void onSaveNewUser(ActionEvent actionEvent)
+    {
+    }
+
+    public void onCancelNewUser(ActionEvent actionEvent)
+    {
+    }
+}
