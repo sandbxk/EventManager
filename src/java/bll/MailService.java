4e85bf49f5c21308e57c103ba85222e42e61b2ca
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-03-29 21:18:09 +0200

    updated but still not working mailservice

--- src/java/bll/MailService.java
+++ src/java/bll/MailService.java
@@ -1,43 +1,65 @@
 package bll;
 
 
-import org.apache.commons.mail.EmailAttachment;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.MultiPartEmail;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class MailService {
 
-    public EmailAttachment setTicketAtch(String rootPath, String eventName)
-    {
-        EmailAttachment attachment = new EmailAttachment();
-        attachment.setPath(rootPath);
-        attachment.setDescription("a demo ticket for a half build ticketguru");
+    private Properties emailProp;
 
-        return attachment;
+    public MailService() throws IOException {
+
+        emailProp = new Properties();
+        emailProp.load(new FileReader("email.properties"));
+
     }
 
-    public MultiPartEmail setEmail(String emailReciever) throws EmailException {
-        MultiPartEmail email = new MultiPartEmail();
-        email.setHostName("smtp.gmail.com");
-        email.addTo(emailReciever);
-        email.setFrom("ticketguru12@gmail.com");
-        email.setSmtpPort(587);
-        email.setAuthentication("ticketguru12@gmail.com", "9Hc0OCi2f");
-        email.setSubject("email service tests");
-        email.setTLS(true);
-        email.setMsg("bare en hurtig test");
-        return email;
+    public void sendMail() throws MessagingException, IOException {
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", 587);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.transport.protocol", "smtp");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailProp.getProperty("username"), emailProp.getProperty("password"));
+            }
+        });
+
+        Message messge = new MimeMessage(session);
+        messge.setSubject("just a proof of concept");
+
+        Address adressTO = new InternetAddress("kasperpro9@gmail.com");
+        messge.setRecipient(Message.RecipientType.TO, adressTO);
+
+        MimeMultipart multiPart = new MimeMultipart();
+
+        MimeBodyPart attachment = new MimeBodyPart();
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        attachment.attachFile(".design/ticketprint/A4 - 1.png");
+        messageBodyPart.setContent("<h1>Email test from mailService!</h1>", "tekst/html");
+        multiPart.addBodyPart(messageBodyPart);
+        multiPart.addBodyPart(attachment);
+
+        messge.setContent(multiPart);
+
+        Transport.send(messge);
     }
 
-    public void sendEmail(EmailAttachment atch, MultiPartEmail email) throws EmailException {
-        email.attach(atch);
-        email.send();
-    }
-
-    public static void main(String[] args) throws EmailException {
+    public static void main(String[] args) throws IOException, MessagingException {
         MailService ms = new MailService();
-        EmailAttachment atch =  ms.setTicketAtch(".design/ticketprint/A4 - 1.png", "test1");
-        MultiPartEmail email = ms.setEmail("kasperpro9@gmail.com");
-        ms.sendEmail(atch, email);
+        ms.sendMail();
     }
 }
