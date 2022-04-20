724afaa64655e68ddc2fb4aebdb897d4841b9434
Mads Mandahl-Barth <mads-barth@live.dk>
2022-04-20 10:33:45 +0200

    conflict fix

--- src/java/bll/ExporterPDF.java
+++ src/java/bll/ExporterPDF.java
@@ -3,24 +3,31 @@ package bll;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 import j2html.TagCreator;
-
+import j2html.attributes.Attr;
+import j2html.tags.specialized.BodyTag;
 import java.io.*;
-import java.util.stream.Stream;
 
 public class ExporterPDF
 {
     public void export() throws IOException
     {
-        var html = TagCreator.body(
-                TagCreator.h1("Hello, World!")
-        ).render();
+        BodyTag body = TagCreator.body
+                (
+                        TagCreator.div(
+                                TagCreator.h1("Hello, World!"),
+                                TagCreator.br(Attr.shortFormFromAttrsString(".class1"))
+                        ),
+                    TagCreator.div(TagCreator.text("a"))
+                );
+
+
 
         File pdfDest = new File("output.pdf");
 
         // pdfHTML specific code
         ConverterProperties converterProperties = new ConverterProperties();
         converterProperties.setImmediateFlush(true);
-        HtmlConverter.convertToPdf(new ByteArrayInputStream(html.getBytes()), new FileOutputStream(pdfDest), converterProperties);
+        HtmlConverter.convertToPdf(new ByteArrayInputStream(body.render().getBytes()), new FileOutputStream(pdfDest), converterProperties);
     }
 
     public static void main(String[] args) throws IOException {
